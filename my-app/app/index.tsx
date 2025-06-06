1c470294df5db767e202d7b6a3febd30c0ced424
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-06-06 22:30:00 +0100

    feat: update layout and index components for improved navigation and styling

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,32 +1,30 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={styles.container}>
-        Hello World
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#25292e",
     alignItems: "center",
     justifyContent: "center",
-    height: "100%",
-    width: "100%",
-    color: "black",
-    textAlign: "center",
-    fontSize: 40,
-    fontWeight: 800,
-    padding: 20,
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
   },
 });
