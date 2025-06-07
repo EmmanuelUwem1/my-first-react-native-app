60ad4fe9987a729984123e5bcef1485c6c64658a
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-06-07 20:22:21 +0100

    feat: implement NotFound screen with navigation link to Home

--- my-app/app/+not-found.tsx
+++ my-app/app/+not-found.tsx
@@ -1,14 +1,13 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { Link, Stack } from "expo-router";
 
-export default function Index() {
+export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{title:"Home"}}></Stack.Screen>
+      <Stack.Screen options={{ title: "Oops! Not Found" }} />
       <View style={styles.container}>
-        <Text style={styles.text}>Home screen</Text>
-        <Link href="/about" style={styles.button}>
-          Go to About screen
+        <Link href="/" style={styles.button}>
+          Go back to Home screen!
         </Link>
       </View>
     </>
@@ -19,12 +18,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#25292e",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
   },
-  text: {
-    color: "#fff",
-  },
+
   button: {
     fontSize: 20,
     textDecorationLine: "underline",
