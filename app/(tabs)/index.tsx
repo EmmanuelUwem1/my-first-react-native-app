124b176c21804538c3de799a39343fa53f051542
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-06-14 22:10:58 +0100

    feat: refactor Index component to include ScrollView for better layout and interactivity

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,16 @@
-import { Text, View, StyleSheet, SafeAreaView, TouchableHighlight } from "react-native";
+import { Text, View, StyleSheet, SafeAreaView, ScrollViewComponent , ScrollView, TouchableHighlight } from "react-native";
 import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.boxContainer}>
-        {[...Array(18)].map((_, index) => (
-          <TouchableHighlight key={index} style={styles.box}></TouchableHighlight>
-        ))}
-      </View>
+      <ScrollView>
+        <View style={styles.boxContainer}>
+          {[...Array(18)].map((_, index) => (
+            <View key={index} style={styles.box}></View>
+          ))}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -19,6 +21,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#25292e",
     alignItems: "center",
     justifyContent: "center",
+    position: "relative",
+    height:"100%"
   },
   text: {
     color: "white",
@@ -34,7 +38,9 @@ const styles = StyleSheet.create({
     flexWrap: "wrap", // Ensures boxes stay within the screen
     gap: 16, // Works in React Native 0.71+
     justifyContent: "center",
-    alignItems:"center"
+    alignItems: "center",
+    paddingTop: 120,
+    paddingBottom:60
   },
   box: {
     backgroundColor: "black",
